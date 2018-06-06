7a76f35f6a2857d2ee4c1a848303d28423841044
Reginaldo Santos <rmsantos@br.ibm.com>
2018-06-05 23:34:43 -0300

    Aula 2 - Organizando o conteúdo

---
+++ src/Home/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inicio = props => {
+    return (
+        <div>
+            <h1>Bem vindo</h1>
+            <p>Este é um jogo de perguntas e respostas</p>
+        </div>
+    )
+}
+
+export default Inicio

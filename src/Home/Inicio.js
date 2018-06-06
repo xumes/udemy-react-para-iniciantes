d7a14c64323c4bb5d481ca59e6416fcfe03eb19e
Reginaldo Santos <rmsantos@br.ibm.com>
2018-06-06 00:36:49 -0300

    aula3 Finalizando o componente Inicio

--- src/Home/Inicio.js
+++ src/Home/Inicio.js
@@ -1,23 +1,33 @@
 import React from 'react'
-import {Card, Button} from 'semantic-ui-react'
+import { Container, Header, Segment, Card, Button } from 'semantic-ui-react'
 
 const Inicio = props => {
     return (
-        <Card>
-            <Card.Header>
-                Quiz
-            </Card.Header>
-            <Card.Meta>
-                Jogo de Perguntas e Respostas
-            </Card.Meta>
-            <Card.Content>
-                Desafie os seus amigos neste incrível jogo de perguntas e respostas. Basta acessar com sua conta do Facebook.
-            </Card.Content>
-            <Card.Content>
-                <Button>Login com Facebook</Button>
-                <Button>Usuário Administrativo</Button>
-            </Card.Content>
-        </Card>
+        <div>
+            <Container>
+                <Segment piled>
+                    Quiz
+                </Segment>
+                <Header as='h2'>
+                    Jogo de Perguntas e Respostas
+                </Header>
+                <p>
+                    Desafie os seus amigos neste incrível jogo de perguntas e respostas. Basta acessar com sua conta do Facebook.
+                </p>
+                <Card fluid>
+                    <Card.Content>
+                        Acesse agora mesmo
+                    </Card.Content>
+                    <Card.Content>
+                        <Button basic color='blue'>Login com Facebook</Button>
+                        <Button basic color='green'>Usuário Administrativo</Button>
+                    </Card.Content>
+                </Card>
+
+            </Container>
+
+
+        </div>
     )
 }
 

f03d1137a6113113a835c338487f55982f3c3d9f
Reginaldo Santos <rmsantos@br.ibm.com>
2018-06-06 23:53:39 -0300

    Criando o componente Ranking

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Inicio from './Home/Inicio'
 import Categorias from './Jogo/Categorias'
 import Perguntas from './Jogo/Perguntas'
 import Resultado from './Jogo/Resultado'
+import Ranking from './Jogo/Ranking'
 
 class App extends Component {
   render() {
@@ -15,7 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Jogo de Perguntas e Respostas</h1>
         </header>
-        <Resultado />
+        <Ranking />
       </div>
     );
   }

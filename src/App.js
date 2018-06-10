93436640bb104ab3df9d71e0539267bbf037e124
Reginaldo Santos <rmsantos@br.ibm.com>
2018-06-09 23:50:03 -0300

    iniciando a navegação com rotas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import Inicio from './Home/Inicio'
 import Categorias from './Jogo/Categorias'
 import Perguntas from './Jogo/Perguntas'
 import Resultado from './Jogo/Resultado'
 import Ranking from './Jogo/Ranking'
+import Header from './Jogo/Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Jogo de Perguntas e Respostas</h1>
-        </header>
-        <Ranking />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Header />
+            <h1 className="App-title">Jogo de Perguntas e Respostas</h1>
+          </header>
+          <Route path='/' exact component={Inicio} />
+          <Route path='/categorias' component={Categorias} />
+          <Route path='/perguntas' component={Perguntas} />
+          <Route path='/resultado' component={Resultado} />
+          <Route path='/ranking' component={Ranking} />
+        </div>
+      </BrowserRouter>
     );
   }
 }

e8f02b7d3770a85a081f3a62ed9bcdb76bc05e3b
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-06-10 00:10:02 -0300

    criando o menu de navegação

--- src/Jogo/Header.js
+++ src/Jogo/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Menu, Image} from 'semantic-ui-react'
 
 const Header = props => {
     return (
-        <h1>Navegação</h1>
+        <Menu>
+            <Menu.Item><strong>Quiz</strong></Menu.Item>
+            <Menu.Item as={Link} to='/'>Home</Menu.Item>
+            <Menu.Item as={Link} to='/categorias'>Categorias</Menu.Item>
+            <Menu.Item as={Link} to='/perguntas'>Perguntas</Menu.Item>
+            <Menu.Item as={Link} to='/resultado'>Resultado</Menu.Item>
+            <Menu.Item as={Link} to='/ranking'>Ranking</Menu.Item>
+            <Menu.Menu position='right'>
+                <Menu.Item><Image avatar src='' />Reginaldo Santos</Menu.Item>
+            </Menu.Menu>
+        </Menu>
     )
 }
 

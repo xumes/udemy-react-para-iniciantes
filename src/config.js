b5b0e48bb52419c3992bb7282276e04f134a6ada
Reginaldo Santos <rmsantos@br.ibm.com>
2018-06-11 23:37:36 -0300

    login no facebook e exibição do usuário na tela

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 import Rebase from 're-base'
 
-const firebaseInfo = {
+const firebaseInfo = firebase.initializeApp({
     apiKey: "AIzaSyC9jZvAduVJgAmgunbHXU1_K8f_yNjGXgA",
     authDomain: "quiz-xumes.firebaseapp.com",
     databaseURL: "https://quiz-xumes.firebaseio.com",
     projectId: "quiz-xumes",
     storageBucket: "quiz-xumes.appspot.com",
     messagingSenderId: "542913374410"
-};
+});
 const db = firebase.database(firebaseInfo)
 const config = Rebase.createClass(db)
 

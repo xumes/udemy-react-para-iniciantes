b3d1d0265af951118d3bf1483eea98d9456ec807
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-06-14 17:38:31 -0300

    deslogando o usuário da sessão

--- src/Jogo/Navegacao.js
+++ src/Jogo/Navegacao.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Menu, Image, Dropdown } from 'semantic-ui-react'
+import { Menu, Image, Dropdown, Icon } from 'semantic-ui-react'
+import firebase from 'firebase'
 
 class Header extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            usuario: ''
+            usuario: '',
+            estaLogado: false
         }
     }
 
@@ -17,9 +19,29 @@ class Header extends Component {
             foto: localStorage.getItem('foto')
         }
 
+
+
         this.setState({
-            usuario: usuarioLogado
+            usuario: usuarioLogado,
+            estaLogado: !!localStorage.getItem('nome')
         })
+    }
+
+    deslogarUsuario() {
+        firebase
+            .auth()
+            .signOut()
+            .then(() => {
+                localStorage.removeItem('nome')
+                localStorage.removeItem('foto')
+                this.setState({
+                    usuario: '',
+                    estaLogado: false
+                })
+            })
+            .catch(err => {
+                console.log('Erro ao deslogar o usuário')
+            })
     }
 
     render() {
@@ -27,7 +49,7 @@ class Header extends Component {
         return (
             <div>
                 <header className="App-header">
-                    <h1 className="App-title">Jogo de Perguntas e Respostas</h1>
+                    <h1 className="App-title">Jogo de Perguntas e Respostas {JSON.stringify(this.state.estaLogado)}</h1>
                     <Menu>
                         <Menu.Item><strong>Quiz</strong></Menu.Item>
                         <Menu.Item as={Link} to='/'>Home</Menu.Item>
@@ -36,13 +58,29 @@ class Header extends Component {
                         <Menu.Item as={Link} to='/resultado'>Resultado</Menu.Item>
                         <Menu.Item as={Link} to='/ranking'>Ranking</Menu.Item>
                         <Menu.Menu position='right'>
-                            <Menu.Item><Image avatar src={foto} /></Menu.Item>
-                            <Dropdown item text={nome}>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item>Sair</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
+
+                            {
+                                this.state.estaLogado &&
+                                <span>
+                                    <Menu.Item><Image avatar src={foto} /></Menu.Item>
+                                    <Dropdown item text={nome}>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item onClick={this.deslogarUsuario}>Sair</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </span>
+
+                            }
+
+                            {
+                                !this.state.estaLogado &&
+                                <Menu.Item><Icon name='user' /></Menu.Item>
+                            }
+
                         </Menu.Menu>
+
+
+
                     </Menu>
                 </header>
             </div>

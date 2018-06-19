fe171250c37e6b3fa179f9ffe50e014a40505e9d
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-06-18 23:54:43 -0300

    exibindo a quantiade de pontos que o jogador fez nesta partida

--- src/Jogo/Resposta.js
+++ src/Jogo/Resposta.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { List, Label, Icon } from 'semantic-ui-react'
 
 const Resposta = props => {
-     const { pergunta, resposta, key} = props
+    const { pergunta, resposta, acertou, key } = props
     return (
         <List.Item key={key}>
             <List.Content floated='left'>
@@ -12,14 +12,15 @@ const Resposta = props => {
                 {pergunta}
             </List.Content>
             <List.Content floated='right'>
-                {resposta}  
-                <Icon name='thumbs up outline' />
+                {acertou && <Icon name='thumbs up outline' />}
+                {!acertou && <Icon name='thumbs down outline' />}
             </List.Content>
+            <List.Content floated='right'>
+                {resposta}
+            </List.Content>
+
         </List.Item>
     )
 }
 
 export default Resposta
-
-//thumbs up outline
-//thumbs down outline

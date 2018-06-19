c0470da2a2bb242b6fad332385b5c02f7a967c58
Reginaldo Santos <rmsantos@br.ibm.com>
2018-06-18 23:54:43 -0300

    exibindo a quantiade de pontos que o jogador fez nesta partida

--- src/Jogo/Resultado.js
+++ src/Jogo/Resultado.js
@@ -4,27 +4,38 @@ import { List, Container } from 'semantic-ui-react'
 import Resposta from './Resposta'
 import Navegacao from './Navegacao'
 
-
+let label= 'pontos'
 
 class Resultado extends Component {
     render() {
-        const { resultado } = this.props.location.state
+        const { resultado, pontos } = this.props.location.state
+        
+        if (pontos == 1){
+            label = 'ponto'
+        }
         return (
             <div>
                 <Navegacao />
                 <h2>Seus resultados</h2>
-                {JSON.stringify(this.props.location.state)}
                 <p>Confira o seu desempenho nesta categoria</p>
+                
                 <Container>
                     <List divided>
                         {
                             Object.keys(resultado)
                                 .map(key => {
-                                    return <Resposta pergunta={resultado[key].pergunta} resposta={resultado[key].resposta} key={key} />
+                                    return (
+                                        <Resposta 
+                                            pergunta={resultado[key].pergunta} 
+                                            resposta={resultado[key].resposta} 
+                                            acertou={resultado[key].acertou}
+                                            key={key} />
+                                    )
                                 })
                         }
                     </List>
                 </Container>
+                <h3>Total: {pontos} {label}</h3>
             </div>
         )
     }
